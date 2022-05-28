0b46978fde6b1d2c340c9af628d0b3ba17094a71
rhonstratos <kizunaai20@gmail.com>
2022-05-28 21:09:13 +0800

    inventory view

--- pages/views/inventory.php
+++ pages/views/inventory.php
@@ -1,14 +1,82 @@
-<?php session_start(); require_once "../includes/config.php"; ?>
+<?php session_start();
+require_once "../includes/config.php"; ?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <?php require_once "../includes/head.php"; ?>
     <title>Inventory</title>
 </head>
+
 <body>
     <?php require_once "../includes/navbar_inventory.php"; ?>
     <div>
-        div 
+        <div class="row">
+            <div class="fit">
+                <h2>Dark Chocolates</h2>
+                <table>
+                    <thead>
+                        <tr>
+                            <th>Name</th>
+                            <th>Price</th>
+                            <th>Quantity</th>
+                            <th>Action</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>asiudgsad</td>
+                            <td>asdaowu87yd</td>
+                            <td>asasdasdyo</td>
+                            <td><button>View</button></td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+            <div class="fit">
+                <h2>Milk Chocolates</h2>
+                <table>
+                    <thead>
+                        <tr>
+                            <th>Name</th>
+                            <th>Price</th>
+                            <th>Quantity</th>
+                            <th>Action</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>asiudgsad</td>
+                            <td>asdaowu87yd</td>
+                            <td>asasdasdyo</td>
+                            <td><button>View</button></td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+            <div class="fit">
+                <h2>White Chocolates</h2>
+                <table>
+                    <thead>
+                        <tr>
+                            <th>Name</th>
+                            <th>Price</th>
+                            <th>Quantity</th>
+                            <th>Action</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>asiudgsad</td>
+                            <td>asdaowu87yd</td>
+                            <td>asasdasdyo</td>
+                            <td><button>View</button></td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
     </div>
 </body>
+
 </html>

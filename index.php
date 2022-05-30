0cdd3c977f4622d3bf67367a53cc895ed9fb4940
rhonstratos <kizunaai20@gmail.com>
2022-05-30 20:34:57 +0800

    index

--- index.php
+++ index.php
@@ -4,6 +4,6 @@ if (phpversion() >= $requirement) {
     echo 'Current PHP version: ' . phpversion();
     header("Location: ./pages/views");
 } else {
-    echo 'Current PHP version: ' . phpversion() . ', but ' . $requirement . ' or above is required <br><br><br>';
+    echo 'Current PHP version: ' . phpversion() . ', but ' . $requirement . ' or above is required <br>Please update your (Linux|XAMPP) server using composer<br><br>';
     echo phpinfo();
 }

228717c8925cb7fb5acbdf4a72da9f75104efed1
rhonstratos <kizunaai20@gmail.com>
2022-05-30 16:55:01 +0800

    php version check

--- index.php
+++ index.php
@@ -1,1 +1,9 @@
-<?php header("Location: ./pages/views");
+<?php
+$requirement = (float) 8.0;
+if (phpversion() >= $requirement) {
+    echo 'Current PHP version: ' . phpversion();
+    header("Location: ./pages/views");
+} else {
+    echo 'Current PHP version: ' . phpversion() . ', but '.$requirement.' or above is required <br><br><br>';
+    echo phpinfo();
+}

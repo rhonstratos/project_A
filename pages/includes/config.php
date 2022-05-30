e3ad266b555c9627062574f9887ee53779388ad2
rhonstratos <kizunaai20@gmail.com>
2022-05-30 16:39:12 +0800

    fix css

--- pages/includes/config.php
+++ pages/includes/config.php
@@ -100,9 +100,9 @@ class Shop
                     <div class="body">
                         <img src="../assets/<?php echo $itemPic;  ?>" class="item-img" alt="item" width="100" height="200">
                         <div class="pad-vertical-1">
-                            <span style="display: none;"><?php echo $itemName; ?></span>
-                            <span style="display: none;"><?php echo $itemDesc; ?></span>
-                            <span style="display: none;"><?php echo $itemPrice; ?></span>
+                            <span style="visibility: hidden;" class="text-center"><?php echo $itemName; ?></span>
+                            <span style="visibility: hidden;"><?php echo $itemDesc; ?></span>
+                            <span style="visibility: hidden;" class="text-center"><?php echo $itemPrice; ?></span>
                         </div>
                         <input value="Add to Cart" type="button" onclick="location.href='#'">
                     </div>
--- src/js/app.js
+++ src/js/app.js
@@ -1,12 +1,12 @@
 function hide(event) {
     var span = event.target.getElementsByTagName("span");
     for (var element of span){
-        element.style.display = "none";
+        element.style.visibility = "hidden";
     }
 }
 function unhide(event) {
     var span = event.target.getElementsByTagName("span");
     for (var element of span){
-        element.style.display = "block";
+        element.style.visibility = "visible";
     }
 }

a8c56719ca7323c3acca33305ddf4f874c8e2ebd
rhonstratos <kizunaai20@gmail.com>
2022-06-01 16:17:07 +0800

    remove bootstrap

--- pages/includes/head.php
+++ pages/includes/head.php
@@ -3,3 +3,6 @@
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <link rel="stylesheet" href="../../src/css/app.css">
 <script src="../../src/js/app.js" charset="UTF-8" defer></script>
+<!--Bootstrap-->
+<link rel="stylesheet" href="../../src/css/bootstrap.min.css">
+<script src="../../src/js/bootstrap.bundle.min.js" charset="UTF-8" defer></script>
--- pages/views/browse.php
+++ pages/views/browse.php
@@ -8,12 +8,23 @@
                 <option value="white_chocolate">White Chocolate</option>
             </select>
             <input type="button" value="Filter" onclick="fillBrowseByCategory()">
-        </div>    
+        </div>
     </div>
+    <button id="myBtn">Open Modal</button>
     <div class="grid" id="browse">
         <?php
         $shop = new Shop();
         $shop->fillShop();
         ?>
+    </div>
+    <!-- The Modal -->
+    <div id="myModal" class="modal">
+
+        <!-- Modal content -->
+        <div class="modal-content">
+            <span class="close">&times;</span>
+            <p>Some text in the Modal..</p>
+        </div>
+
     </div>
 </div>

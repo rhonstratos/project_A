44a34e5bc4a06a4b58ccce3873569d4d8f148ca4
rhonstratos <kizunaai20@gmail.com>
2022-06-04 20:04:27 +0800

    removed sesarchbar

--- pages/includes/navbar.php
+++ pages/includes/navbar.php
@@ -4,12 +4,6 @@ $user = new User(); ?>
     <div class="row">
         <a class="nostyle" style="width:100%" href="./home.php">Willy Wonka's Chocolate Factory</a>
     </div>
-    <div class="row" style="width: max-content;">
-        <form action="./home.php" method="get" class="row search-form" style="width: 200px;" >
-            <input type="text" class="search pad-horizontal-1"placeholder="Search" name="search" id="search">
-            <button type="submit" class="search-btn"><img src="../assets/search.png" width="10" alt="Search"></button>
-        </form>
-    </div>
     <div class="row-end">
         <a href="./checkout.php" class="nav-item nostyle">Checkout Cart</a>
         <a href="./purchases.php" class="nav-item nostyle">Purchase History</a>
--- pages/includes/navbar_inventory.php
+++ pages/includes/navbar_inventory.php
@@ -3,12 +3,6 @@
     <div>
         <span>Willy Wonka's Chocolate Factory</span>
     </div>
-    <div>
-        <form action="./inventory.php" method="get" class="row search-form">
-            <input type="text" class="search pad-horizontal-1" placeholder="Search" name="search" id="search">
-            <button type="submit" class="search-btn"><img src="../assets/search.png" width="10" alt="Search"></button>
-        </form>
-    </div>
     <div class="row-end">
         <a href="./registerItem.php" class="nav-item nostyle">Register Item</a>
         <a href="./inventory.php" class="nav-item nostyle">Inventory</a>
